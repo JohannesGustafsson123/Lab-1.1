ea7f3d1e172fa6d02e7d7981c9e1b7be00e800fb
Johannes Gustafsson <johannes.gustafsson@elev.ga.ntig.se>
2024-09-16 09:45:15 +0200

    Oopsies

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Vad heter du? ");
