0e61a1ca0173867be2bfe0233762598230da9b32
Johannes Gustafsson <johannes.gustafsson@elev.ga.ntig.se>
2024-09-16 09:16:49 +0200

    2nd commit

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-public class Main {
+import java.util.Scanner;
+
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Vad heter du? ");
+        String namn = scanner.nextLine();
+        System.out.println("Hej " + namn + "!");
     }
 }
